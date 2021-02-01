611a0d7fbc700c30b3405ea5f47571457d9c13b0
jules fagnoni <fagnonijules@gmail.com>
2021-02-01 15:09:15 +0100

    lists created by default with user creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'pages#home'
   get "books/search/:query", to: 'pages#books'
   get "lists", to: "list#index"
